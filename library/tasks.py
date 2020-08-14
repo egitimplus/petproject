70377e0b6be4436761e605d10cac889eb48a6ee8
Emre Çevik <info@internet.com.tr>
2020-08-14 22:18:25 +0300

    analyse fixed

--- library/tasks.py
+++ library/tasks.py
@@ -1,32 +1,7 @@
-# Create your tasks here
 from __future__ import absolute_import, unicode_literals
-
 from celery import shared_task
-from food.models import Food
 
 
 @shared_task
 def add(x, y):
     return x + y
-
-
-@shared_task
-def mul(x, y):
-    return x * y
-
-
-@shared_task
-def xsum(numbers):
-    return sum(numbers)
-
-
-@shared_task
-def count_widgets():
-    return Food.objects.count()
-
-
-@shared_task
-def rename_widget(food_id, name):
-    w = Food.objects.get(id=food_id)
-    w.name = name
-    w.save()
