fcd078c4db2d51ec40193ea0c4e966e623b7a396
Emre Çevik <info@internet.com.tr>
2020-08-15 01:45:20 +0300

    analyse fixed

--- petproject/pet_celery.py
+++ petproject/pet_celery.py
@@ -1,14 +1,24 @@
 from __future__ import absolute_import, unicode_literals
+
 import os
+
 from celery import Celery
-import sys
-import django
-from django.conf import settings
 
-
-app = Celery('petproject')
-app.config_from_object('django.conf:settings', namespace='CELERY')
+# set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'petproject.settings')
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../petproject')))
-django.setup()
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+app = Celery('backend')
+
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load task modules from all registered Django app configs.
+app.autodiscover_tasks()
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
