20a08633e0a0a17df73ffd5afce73dfb25bcc932
Emre Çevik <info@internet.com.tr>
2020-08-23 22:57:16 +0300

    analyse fixed

--- petproject/settings.py
+++ petproject/settings.py
@@ -13,7 +13,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import environ
 
-env = environ.Env()
+env = environ.Env(
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
